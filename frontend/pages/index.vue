b1dcf7916d02f061e64215fc8bc146968acdda3e
Fenish <sohretalhadev@outlook.com>
2024-08-02 01:41:37 +0300

    Add loading text

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,16 +4,21 @@
 	>
 		<h1 class="text-3xl text-white">Endpoint Listesi</h1>
 		<div
+			v-if="endpoints.length !== 0"
 			class="flex gap-x-10 w-full items-center justify-center mt-5 flex-wrap gap-y-5"
 		>
 			<div
-				class="py-5 w-52 flex items-center justify-center bg-green-400 border-2 border-green-500 rounded-sm group hover:bg-green-600 transition-colors duration-300 cursor-pointer"
+				class="py-5 min-w-52 px-5 flex items-center justify-center bg-green-400 border-2 border-green-500 rounded-sm group hover:bg-green-600 transition-colors duration-300 cursor-pointer"
 				v-for="endpoint in endpoints"
 				:key="endpoint"
 				@click="navigateTo(endpoint.component)"
 			>
 				<span class="text-xl select-none">{{ endpoint.endpoint }}</span>
 			</div>
+		</div>
+
+		<div v-else class="mt-5">
+			<span class="text-white text-3xl">Yükleniyor...</span>
 		</div>
 	</div>
 </template>
