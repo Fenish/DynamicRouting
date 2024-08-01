1ba88f8d916fa6ce99bb1b9164d54eb7170e1f12
Fenish <sohretalhadev@outlook.com>
2024-08-02 02:28:42 +0300

    Create example component

---
+++ frontend/customPages/users.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="w-full min-h-screen flex items-center justify-center">
+		<div class="flex flex-col gap-y-5 w-1/2 items-center">
+			<span class="text-5xl font-extrabold">Users Sayfası</span>
+			<span
+				>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+				veniam, earum sunt ad impedit, assumenda modi, tenetur
+				doloremque sit ipsum repellendus placeat. Vel vero dolor
+				deserunt nulla aperiam. Porro, dicta. Lorem ipsum dolor sit amet
+				consectetur adipisicing elit. Mollitia perferendis, odio
+				pariatur, veniam eius architecto dolores sed blanditiis,
+				voluptas totam delectus maiores exercitationem quod! Vel placeat
+				perferendis obcaecati dolores at?
+			</span>
+		</div>
+	</div>
+</template>
