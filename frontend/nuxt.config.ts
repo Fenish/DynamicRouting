678225d9ad026faf9da86156f3ad94b9ee18b8f1
Fenish <sohretalhadev@outlook.com>
2024-08-02 02:28:23 +0300

    Register dynamic routing components

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,12 +2,11 @@
 export default defineNuxtConfig({
 	compatibilityDate: "2024-04-03",
 	devtools: { enabled: true },
-	modules: [
-		"@nuxtjs/tailwindcss"
-	],
+	modules: ["@nuxtjs/tailwindcss"],
 	runtimeConfig: {
 		public: {
 			API_URL: process.env.API_URL,
 		},
 	},
+	components: [{ path: "~/customPages", pathPrefix: true, global: true }],
 });
